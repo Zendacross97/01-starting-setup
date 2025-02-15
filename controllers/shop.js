4aa08bf598ad6017877c9983072ca6e6cbffe57b
Zendacross97 <sidhchakraboty66@gmail.com>
2025-02-15 21:13:16 +0530

    refactor: update product handling to use Sequelize methods

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,10 +2,10 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
         path: '/products'
       });
@@ -15,12 +15,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) =>{
-      console.log(product);
+  // Product.findByPk(prodId)//in this case dont use [0] because it is not an array
+  Product.findAll({where: {id: prodId}})
+    .then(product =>{
       res.render('shop/product-detail', {
         product: product[0],
-        pageTitle: product.title,
+        pageTitle: product[0].title,
         path: '/products'
       });
     })
@@ -28,10 +28,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/index', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Shop',
         path: '/'
       });
